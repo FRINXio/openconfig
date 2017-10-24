436837e0fabd0de573548248c7237880b79a2b74
Dana Kutenicsova <dkutenicsova@frinx.io>
2017-10-24 14:15:15 +0200

    Added copyright headers, so that it compiles.
    Change-Id: I517fbd421ccceb81d15a72ce2a7dfbaa3a08eb10
    Signed-off-by: Dana Kutenicsova <dkutenicsova@frinx.io>

--- network-instance/src/main/java/io/frinx/openconfig/network/instance/NetworInstance.java
+++ network-instance/src/main/java/io/frinx/openconfig/network/instance/NetworInstance.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright © 2017 Frinx and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
 package io.frinx.openconfig.network.instance;
 
 import org.opendaylight.yang.gen.v1.http.openconfig.net.yang.network.instance.rev170228.network.instance.top.network.instances.NetworkInstanceKey;
