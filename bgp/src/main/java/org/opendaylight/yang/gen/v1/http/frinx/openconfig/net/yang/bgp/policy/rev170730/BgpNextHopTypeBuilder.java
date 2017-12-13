5ef4104bedd07600433f9f273b6ff88c99db4c72
Michal Cmarada <mcmarada@frinx.io>
2017-12-13 09:29:18 +0100

    Add frinx-openconfig-bgp-policy model
    - adding new model for bgp-policy + builders
    
    
    Change-Id: I2a5e6f2fd01b2bc6d15395603e7e85025dca9ca7
    Signed-off-by: Michal Cmarada <mcmarada@frinx.io>

---
+++ bgp/src/main/java/org/opendaylight/yang/gen/v1/http/frinx/openconfig/net/yang/bgp/policy/rev170730/BgpNextHopTypeBuilder.java
@@ -0,0 +1,31 @@
+package org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.bgp.policy.rev170730;
+
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.types.inet.rev170403.IpAddress;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.types.inet.rev170403.IpAddressBuilder;
+
+/**
+ * The purpose of generated class in src/main/java for Union types is to create new instances of unions from a string representation.
+ * In some cases it is very difficult to automate it since there can be unions such as (uint32 - uint16), or (string - uint32).
+ *
+ * The reason behind putting it under src/main/java is:
+ * This class is generated in form of a stub and needs to be finished by the user. This class is generated only once to prevent
+ * loss of user code.
+ *
+ */
+public class BgpNextHopTypeBuilder {
+
+    public static BgpNextHopType getDefaultInstance(java.lang.String defaultValue) {
+        try {
+            final BgpNextHopType.Enumeration nextHopEnum = BgpNextHopType.Enumeration.valueOf(defaultValue.toUpperCase());
+            return new BgpNextHopType(nextHopEnum);
+        } catch (final IllegalArgumentException e) {
+            try {
+                final IpAddress ipAddress = IpAddressBuilder.getDefaultInstance(defaultValue);
+                return new BgpNextHopType(ipAddress);
+            } catch (final IllegalArgumentException e1) {
+                throw new IllegalArgumentException("Cannot create BgpNextHopType from " + defaultValue);
+            }
+        }
+    }
+
+}
--- types/src/main/java/org/opendaylight/yang/gen/v1/http/frinx/openconfig/net/yang/types/inet/rev170403/IpPrefixBuilder.java
+++ types/src/main/java/org/opendaylight/yang/gen/v1/http/frinx/openconfig/net/yang/types/inet/rev170403/IpPrefixBuilder.java
@@ -1,5 +1,8 @@
 package org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.types.inet.rev170403;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * The purpose of generated class in src/main/java for Union types is to create new instances of unions from a string representation.
  * In some cases it is very difficult to automate it since there can be unions such as (uint32 - uint16), or (string - uint32).
@@ -11,8 +14,31 @@ package org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.types.inet.r
  */
 public class IpPrefixBuilder {
 
-    public static IpPrefix getDefaultInstance(java.lang.String defaultValue) {
-        throw new java.lang.UnsupportedOperationException("Not yet implemented");
+    private static final Pattern IPV4_PATTERN = Pattern.compile("(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])/(([0-9])|([1-2][0-9])|(3[0-2]))");
+    private static final Pattern IPV6_PATTERN1 = Pattern.compile("((:|[0-9a-fA-F]{0,4}):)([0-9a-fA-F]{0,4}:){0,5}((([0-9a-fA-F]{0,4}:)?(:|[0-9a-fA-F]{0,4}))|(((25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])))(/(([0-9])|([0-9]{2})|(1[0-1][0-9])|(12[0-8])))");
+    private static final Pattern IPV6_PATTERN2 = Pattern.compile("(([^:]+:){6}(([^:]+:[^:]+)|(.*\\..*)))|((([^:]+:)*[^:]+)?::(([^:]+:)*[^:]+)?)(/.+)");
+
+    private IpPrefixBuilder() {
+
     }
+
+    public static IpPrefix getDefaultInstance(final String defaultValue) {
+        final Matcher ipv4Matcher = IPV4_PATTERN.matcher(defaultValue);
+
+        if (ipv4Matcher.matches()) {
+            if (IPV6_PATTERN1.matcher(defaultValue).matches() && IPV6_PATTERN2.matcher(defaultValue).matches()) {
+                throw new IllegalArgumentException(
+                    String.format("Cannot create IpPrefix from \"%s\", matches both %s and %s",
+                        defaultValue, Ipv4Address.class.getSimpleName(), Ipv6Address.class.getSimpleName()));
+
+            }
+            return new IpPrefix(new Ipv4Prefix(defaultValue));
+        } else if (IPV6_PATTERN1.matcher(defaultValue).matches() && IPV6_PATTERN2.matcher(defaultValue).matches()) {
+            return new IpPrefix(new Ipv6Prefix(defaultValue));
+        } else {
+            throw new IllegalArgumentException("Cannot create IpPrefix from " + defaultValue);
+        }
+    }
+
 
 }
