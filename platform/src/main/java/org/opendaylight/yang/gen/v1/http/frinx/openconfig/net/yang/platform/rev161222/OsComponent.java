17416c171c91813fe42e70f45fc8504c70418819
lkontur <lkontur@frinx.com>
2018-07-09 16:10:46 +0200

    add ComponentKey constant
    Change-Id: I8664fa30968467c23a20da090dcfcf53b640140c

---
+++ platform/src/main/java/org/opendaylight/yang/gen/v1/http/frinx/openconfig/net/yang/platform/rev161222/OsComponent.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright © 2017 Frinx and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.platform.rev161222;
+
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.platform.rev161222.platform.component.top.components.ComponentKey;
+
+public class OsComponent {
+    public static final String OS_NAME = "OS";
+    public static final ComponentKey OS_KEY = new ComponentKey(OS_NAME);
+}
