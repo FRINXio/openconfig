1bdfda20d150a30911beea0d04aa311a5962acfd
Martin Sunal <msunal@frinx.io>
2019-01-22 23:30:34 +0100

    Added missing YANGs to generate_html.sh
    Change-Id: I308fc19190d0a42c5e6b395c809e4ac05c09adc9
    Signed-off-by: Martin Sunal <msunal@frinx.io>
    (cherry picked from commit 2854646516abdbe179c694dd02f290913fb2ff80)

--- generate_html.sh
+++ generate_html.sh
@@ -4,13 +4,21 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $DIR
 
 # download dependencies
+wget https://raw.githubusercontent.com/opendaylight/mdsal/stable/carbon/model/ietf/ietf-yang-types-20130715/src/main/yang/ietf-yang-types%402013-07-15.yang -O ietf-yang-types@2013-07-15.yang
+wget https://raw.githubusercontent.com/opendaylight/mdsal/stable/carbon/model/ietf/ietf-interfaces/src/main/yang/ietf-interfaces.yang -O ietf-interfaces.yang
+wget https://raw.githubusercontent.com/opendaylight/mdsal/stable/carbon/model/iana/iana-if-type-2014-05-08/src/main/yang/iana-if-type%402014-05-08.yang -O iana-if-type@2014-05-08.yang
+wget https://raw.githubusercontent.com/opendaylight/mdsal/stable/carbon/model/ietf/ietf-inet-types-2013-07-15/src/main/yang/ietf-inet-types%402013-07-15.yang -O ietf-inet-types@2013-07-15.yang
 wget https://raw.githubusercontent.com/opendaylight/mdsal/stable/carbon/model/ietf/ietf-topology/src/main/yang/network-topology%402013-10-21.yang -O network-topology@2013-10-21.yang
 wget https://raw.githubusercontent.com/opendaylight/mdsal/stable/carbon/model/yang-ext/src/main/yang/yang-ext.yang -O yang-ext.yang
 
 # resolved_yangs.txt contains list of all YANG models ordered based on their imports
 # -W none ignore WARNINGS
-pyang -f jstree network-topology@2013-10-21.yang yang-ext.yang $(cat resolved_yangs.txt) -W none > index.html
+pyang -f jstree ietf-yang-types@2013-07-15.yang ietf-interfaces.yang iana-if-type@2014-05-08.yang ietf-inet-types@2013-07-15.yang network-topology@2013-10-21.yang yang-ext.yang $(cat resolved_yangs.txt) -W none > index.html
 
 # remove dependencies
+rm ietf-yang-types@2013-07-15.yang
+rm ietf-interfaces.yang
+rm ietf-inet-types@2013-07-15.yang
+rm iana-if-type@2014-05-08.yang
 rm network-topology@2013-10-21.yang
 rm yang-ext.yang
