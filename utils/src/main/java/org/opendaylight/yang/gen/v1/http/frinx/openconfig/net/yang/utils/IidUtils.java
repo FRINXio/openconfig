5c388bcd551ea890f28ecdf77a894ac35ab5f983
Adam Chmara <achmara@frinx.io>
2019-01-29 13:37:53 +0100

    FI-779: Simplified creation of instances using IIDs and keys
    Signed-off-by: Adam Chmara <achmara@frinx.io>
    Change-Id: I9ac116953f601df9a6ac428261627f2c0702d335

---
+++ utils/src/main/java/org/opendaylight/yang/gen/v1/http/frinx/openconfig/net/yang/utils/IidUtils.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright © 2018 FRINX s.r.o. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the FRINX ODL End User License Agreement which accompanies this distribution,
+ * and is available at https://frinx.io/wp-content/uploads/2017/01/EULA_ODL_20170104_v102.pdf
+ */
+
+package org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.utils;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.reflect.TypeToken;
+import org.opendaylight.yangtools.yang.binding.*;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class IidUtils {
+
+    private IidUtils() {
+    }
+
+    /**
+     * Creates unique identifier - path to the node that uniquely identifies all the node's parent nodes, using
+     * full path to target node and mandatory keys (doesn't have to be ordered).
+     *
+     * @param iid  InstanceIdentifier without keys including full path to target node.
+     * @param keys Identifiers (keys) assignable to specified InstanceIdentifier
+     * @throws NullPointerException informing about missing key - if the necessary keys are missing or doesn't match iid
+     * @return InstanceIdentifier
+     */
+    public static InstanceIdentifier<?> createIid(InstanceIdentifier<?> iid,
+                                                  Identifier<? extends Identifiable<?>>... keys) {
+
+        List<InstanceIdentifier.PathArgument> instanceIdentifier = Lists.newArrayList();
+
+        Map<String, Identifier<? extends Identifiable<?>>> keysMap = Maps.newHashMap();
+
+        for (Identifier<? extends Identifiable<?>> key : keys) {
+
+            Class<? extends Identifier> kClass = key.getClass();
+            Optional<String> typeClass = TypeToken.of(kClass).getTypes().interfaces()
+                    .stream()
+                    .filter(t -> t.getRawType().equals(Identifier.class))
+                    .map(TypeToken::toString)
+                    .map(tS -> tS.substring(tS.lastIndexOf("<") + 1, tS.lastIndexOf(">")))
+                    .map(String::trim)
+                    .findFirst();
+
+            Preconditions.checkArgument(typeClass.isPresent(),
+                    "Unable to determine list type for key: %s ", kClass);
+
+            keysMap.put(typeClass.get(), key);
+        }
+
+        for (InstanceIdentifier.PathArgument pathArgument : iid.getPathArguments()) {
+            Class<? extends DataObject> type = pathArgument.getType();
+
+            if (Identifiable.class.isAssignableFrom(type)) {
+                Preconditions.checkNotNull(keysMap.get(type.getName()),
+                        "Key for %s is missing", type.getName());
+                instanceIdentifier.add(new InstanceIdentifier.IdentifiableItem(type, keysMap.get(type.getName())));
+            } else {
+                instanceIdentifier.add(new InstanceIdentifier.Item<>(type));
+            }
+        }
+
+        return InstanceIdentifier.create(instanceIdentifier);
+    }
+
+}
+
---
+++ utils/src/test/java/IidUtilsTest.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright © 2018 FRINX s.r.o. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the FRINX ODL End User License Agreement which accompanies this distribution,
+ * and is available at https://frinx.io/wp-content/uploads/2017/01/EULA_ODL_20170104_v102.pdf
+ */
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.interfaces.rev161222.subinterfaces.top.Subinterfaces;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.interfaces.rev161222.subinterfaces.top.subinterfaces.Subinterface;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.interfaces.rev161222.subinterfaces.top.subinterfaces.SubinterfaceKey;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.interfaces.rev161222.subinterfaces.top.subinterfaces.subinterface.Config;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.interfaces.rev161222.interfaces.top.interfaces.Interface;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.interfaces.rev161222.interfaces.top.Interfaces;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.interfaces.rev161222.interfaces.top.interfaces.InterfaceKey;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.interfaces.juniper.extention.rev181204.Config1;
+import org.opendaylight.yang.gen.v1.http.frinx.openconfig.net.yang.utils.IidUtils;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+public class IidUtilsTest {
+
+    private InstanceIdentifier WO_KEYS_ID = InstanceIdentifier.create(Interfaces.class)
+            .child(Interface.class)
+            .child(Subinterfaces.class)
+            .child(Subinterface.class)
+            .child(Config.class)
+            .augmentation(Config1.class);
+
+    private InstanceIdentifier EXPECTED_IID = InstanceIdentifier.create(Interfaces.class)
+            .child(Interface.class, new InterfaceKey("GigabitEthernet0/0/0/0"))
+            .child(Subinterfaces.class)
+            .child(Subinterface.class, new SubinterfaceKey(0L))
+            .child(Config.class)
+            .augmentation(Config1.class);
+
+    @Test
+    public void testCreateInstanceIdentifierWithKeys() {
+        InstanceIdentifier actualIID = IidUtils
+                .createIid(WO_KEYS_ID,
+                        new InterfaceKey("GigabitEthernet0/0/0/0"),
+                        new SubinterfaceKey(0L));
+
+        Assert.assertEquals(EXPECTED_IID, actualIID);
+        Assert.assertEquals(EXPECTED_IID.getTargetType(), actualIID.getTargetType());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testCreateInstanceMissingKey () {
+      IidUtils.createIid(WO_KEYS_ID, new InterfaceKey("GigabitEthernet0/0/0/0"));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testCreateInstanceNonMatchingKey () {
+        InstanceIdentifier woKeysIID = InstanceIdentifier.create(Interfaces.class)
+                .child(Interface.class);
+        IidUtils.createIid(woKeysIID, new SubinterfaceKey(0L));
+    }
+
+}
